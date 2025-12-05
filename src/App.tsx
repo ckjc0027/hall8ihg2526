4ec82eeeebe295f2186f4d86eb72679662b52e8e
ckjc0027 <calv0027@e.ntu.edu.sg>
2025-12-05 23:23:56 +0800

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import TournamentManager from './tournament-manager';
+// Import the styling needed for your component (optional, but good practice)
+import './App.css';
+
+function App() {
+  // We return the TournamentManager component, which is your entire application.
+  return (
+    <TournamentManager />
+  );
+}
+
+export default App;
